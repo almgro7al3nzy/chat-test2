da53ea238f115b9b7c02f05614605e233d8e5420
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-16 12:40:57 +0300

    Update socketContext.js

--- src/socketContext.js
+++ src/socketContext.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 const SocketContext = React.createContext()
 
 const SocketProvider = ({ children }) => {
-    const ENDPOINT = 'https://socket-chat-ak.herokuapp.com/';
+    const ENDPOINT = 'https://devf5r-chat.herokuapp.com/';
     const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] })
     return (
         <SocketContext.Provider value={socket}>
