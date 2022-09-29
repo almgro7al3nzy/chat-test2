42e274f4c2b9fe433286cfa6b0d76d73d7d98b85
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-29 04:16:47 +0300

    d

--- index.js
+++ index.js
@@ -1,167 +1,115 @@
-const WebSocket = require("ws");
-const mysql = require("mysql");
-const ws = new WebSocket.Server({port:8008});
-let ALL_USER=[];
-let ALL_ROOM=[];
-ws.on("connection",function connect(websocket,req){
-   websocket.on("message",function incoming(message){
-      console.log(JSON.parse(message));
-      message=JSON.parse(message);
-      switch (message.code){
-         case "member_login":
-            login(message.memberCode,message.memberAlias);
-            break;
-         case "create_room":
-            message.members.sort(function (a,b){
-               return a.memberCode-b.memberCode;
-            });
-            ROOM_ID=createRoom(message.members);
-            break;
-         case "send_chat":
-            sendChat(message.room_id,message.send_memberCode);
-            break;
-         case "room_member_insert":  //방에 회원 추가
-            roomMemberInsert(message.room_id, message.members);
-            break;
-      }
-   });
-   function roomMemberInsert(room_id, members) {
-      let ROOM_ID = "";
-      //DATABASE에 room 정보를 insert
-      const conn = mysql.createConnection({
-         host: "localhost",
-         user: "root",
-         password: "",
-         database: "kakaotalk"
-      });
+require('dotenv').config()
 
-      let sql = " select members, members_name from room where roomCode='" +room_id+ "' ";
-      conn.query(sql, function(err, rows, fields){
-         if(rows && rows.length > 0) { //동일한 회원들이 참여하는 방이 이미 존재하는 경우
-            let this_members = rows[0].members;
-            let this_members_name = rows[0].members_name;
+const express = require("express");   
+const socketio = require("socket.io"); 
+const http = require("http");
+const { ExpressPeerServer } = require('peer');
+const schedule = require('node-schedule');
+const controlRooms = require("./controllers/controlRooms"); 
 
-            let this_members_arr = this_members.split(",");
-            let this_members_name_arr = this_members_name.split(",");
+const twilioObj = {
+    username : null,
+    cred : null 
+}
 
-            for(let q=0; q < this_members_arr.length; q++) {
-               let memberCode = this_members_arr[q];
-               let memberAlias = this_members_name_arr[q];
+// Voice chat uses turn server, not required locally 
+if(process.env.USE_TWILIO==="yes") { 
+    const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    client.tokens.create().then(token => {
+        twilioObj.username = token.username;
+        twilioObj.cred = token.password; 
+    });
 
-               let tmpMember = {"memberCode": memberCode,"memberAlias": memberAlias}
-               members.push(tmpMember);
-            }
+    //every 12 hours 
+    schedule.scheduleJob("*/12 * * *",()=>{
+        console.log("CRON running"); 
+        const client = require('twilio')(process.env.accountSid, process.env.authToken);
+        client.tokens.create().then(token => {
+            twilioObj.username = token.username;
+            twilioObj.cred = token.password; 
+        });
+        controlRooms.deQRoom();
+    })
+}
 
-            members.sort(function(a,b){  //memberCode 기준으로 오름차순 정렬 (1,2,3,4...10)
-               return a.memberCode - b.memberCode;
-            });
+const cors = require('cors');
+const app = express(); 
 
-            let all_member = "";
-            let all_member_name = "";
-            members.forEach(function(element, index){
-               if(!all_member) {
-                  all_member = element.memberCode
-                  all_member_name = element.memberAlias
-               }
-               else {
-                  all_member += "," + element.memberCode
-                  all_member_name += "," + element.memberAlias
-                  //3,2,1,4
-               }
-            });
+const router = require("./controllers/chatController");
+const server = http.createServer(app);
+const io = socketio(server); 
 
-            sql = " update room set members='"+all_member+"', members_name='"+all_member_name+"' where roomCode='"+room_id+"' ";
-            conn.query(sql, function(err){console.log(err);});
+app.use(cors());
+app.use(router); 
 
-            ALL_ROOM.forEach(function(element, index) {
-               if(element.id == room_id) {  //맞는 방 찾기
-                  element.members = members;
-               }
-            });
+const roomRouter = require("./routes/room");
+app.use("/",roomRouter);
 
-            let data = {"code": "room_member_inserted", "room_id": room_id, "members": members};
-            sendMessage(data);
-         }
-      });
-   }
-   function login(memberCode,memberAlias){
-      let member_data={
-         "memberCode":memberCode,
-         "memberAlias":memberAlias,
-         "ws":websocket
-      };
-      ALL_USER.push(member_data);
-      console.log("Login OK");
-   }
-   function createRoom(members){
-      let ROOM_ID="";
-      const conn=mysql.createConnection({
-         host:"localhost",
-         user:"",
-         password:"",
-         database:""
-      });
-      let all_member="";
-      members.forEach(function (element,index){
-         if(!all_member){
-            all_member=element.memberCode
-         }else{
-            all_member+=","+element.memberCode
-         }
-      });
-      let sql="select roomCode from room where members='"+all_member+"'";
-      conn.query(sql,function (err,rows,fields){
-         if(rows && rows.length>0){
-            ROOM_ID=rows[0].roomCode;
-         }else{
-            sql="INSERT INTO room(members) values('"+all_member+"')";
-            conn.query(sql,function(){});
-            sql="select max(roomCode) as roomCode from room";
-            conn.query(sql,function(err,rows,fields){
-               ROOM_ID=rows[0].roomCode;
-            });
-         }
-         conn.end();
-         createRoomStep2(ROOM_ID,members);
-         return ROOM_ID;
-      });
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/'
+});
+app.use('/peerjs', peerServer);
 
-   }
-   function createRoomStep2(t_ROOM_ID,t_members){
-      let room_data={
-         "id":t_ROOM_ID,
-         "members":t_members
-      }
-      let findSameRoomid = ALL_ROOM.filter(function (element){
-         return element.id == t_ROOM_ID;
-      });
-      if(findSameRoomid.length==0){
-         ALL_ROOM.push(room_data);
-      }
-      console.log("createRoom OK");
-      sendRoomInfo(t_ROOM_ID);
-   }
-   function sendRoomInfo(t_ROOM_ID){
-      let data={"code":"send_roominfo","room_id":t_ROOM_ID};
-      sendMessage(data);
-      console.log("sendRoomInfo OK");
-   }
-   function sendMessage(msg){
-      websocket.send(JSON.stringify(msg));
-   }
-   function sendChat(room_id,memberCode){
-      ALL_ROOM.forEach(function (element,index){
-         if(element.id==room_id){
-            element.members.forEach(function(member,memberindex){
-               ALL_USER.forEach(function (user,userindex){
-                  if(member.memberCode==user.memberCode){
-                     let data={"code":"arrive_new_message","room_id":"room_id"};
-                     user.ws.send(JSON.stringify(data));
-                     console.log("arrive_new_message_OK");
-                  }
-               })
-            })
-         }
-      })
-   }
+const { 
+    addUser,removeUser,getUser,getUsersInRoom,
+    getUsersInVoice, addUserInVoice, removeUserInVoice 
+} = require("./controllers/userController"); 
+
+io.on('connection', socket => { 
+
+    socket.on('join',({name,room},callBack)=>{ 
+
+        const user = addUser({id:socket.id,name,room});  //destructuring the object 
+        if(user.error) return callBack(user.error); 
+        socket.join(user.room) //joins a user in a room 
+        socket.emit('message',{user:'admin', text:`Welcome ${user.name} in room ${user.room}.`}); //send to user
+        socket.emit('usersinvoice-before-join',{users:getUsersInVoice(user.room)});
+        socket.broadcast.to(user.room).emit('message',{user:'admin', text:`${user.name} has joined the room`}); //sends message to all users in room except this user
+        io.to(user.room).emit('users-online', { room: user.room, users: getUsersInRoom(user.room) });
+        //console.log(getUsersInRoom(user.room)); 
+        callBack(twilioObj); // passing no errors to frontend for now 
+        //callBack(); 
+    }); 
+
+    
+    socket.on('user-message',(message,callBack)=>{ //receive an message with eventName user-message 
+        const user = getUser(socket.id); 
+        io.to(user.room).emit('message',{user:user.name, text:message }); //send this message to the room 
+        
+        callBack(); 
+    }); 
+
+    socket.on('join-voice',({name,room},callBack)=>{
+        io.to(room).emit('add-in-voice',{id:socket.id,name:name}); 
+        addUserInVoice({id:socket.id,name,room}); 
+        callBack(); 
+    }); 
+    socket.on('leave-voice',({name,room},callBack)=>{
+
+        io.to(room).emit('remove-from-voice',{id:socket.id,name:name}); 
+        removeUserInVoice(socket.id); 
+        callBack(); 
+    }); 
+
+    socket.on('disconnect', () => {
+       
+        const user = removeUser(socket.id);
+        if(user) { 
+            io.to(user.room).emit('message',{user:'admin', text:`${user.name} left the chat` }); //send this message to the room 
+            io.to(user.room).emit('users-online', { room: user.room, users: getUsersInRoom(user.room) });
+            removeUserInVoice(user.id); 
+            socket.broadcast.to(user.room).emit('remove-from-voice',{id:socket.id,name:user.name}); 
+        }
+    });
+    
+
+});
+
+
+
+
+const PORT = process.env.PORT || 5000; 
+server.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`); 
 });
